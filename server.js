49c0eee5fa1f711d8d5a3cc54a463c1edaef6b67
neocheekiong <neocheekiong@gmail.com>
2020-05-19 22:00:49 +0800

    lab edit complete

---
+++ server.js
@@ -0,0 +1,67 @@
+/**
+ * Dependencies
+ */
+const express = require('express');
+const methodOverride = require('method-override');
+const scientists = require('./models/index');
+
+/**
+ * Configuration
+ */
+const app = express();
+const port = 3000;
+app.set('view engine', 'ejs');
+
+/**
+ * Middleware
+ */
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+app.use(methodOverride('_method'));
+
+/**
+ * Routes
+ */
+app.get('/new', (request, response) => {
+    response.render('new.ejs');
+});
+
+app.post('/new', (request, response)=>{
+    scientists.push(request.body);
+    response.redirect('/index');
+});
+
+app.get('/index', (request, response) => {
+    response.render('index.ejs', {
+        data: scientists
+    });
+});
+
+app.delete('/index/:index', (request, response) => {
+    scientists.splice(request.params.index, 1);
+    response.redirect('/index');
+});
+
+app.put('/index/:index', (request, response) => {
+    scientists[request.params.index] = request.body;
+    response.redirect('/index');
+});
+
+app.get('/index/:index/edit', (request, response) => {
+    response.render('edit.ejs', { 
+        scientist: scientists[request.params.index],
+        index: request.params.index
+    });
+});
+
+
+
+/**
+ * Port Listener
+ */
+app.listen(port, () => 
+{
+    console.log('Discovering new worlds on port', port);
+});
