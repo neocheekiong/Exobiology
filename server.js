8f680e8251ef357e04eb234f09b29ae714c54397
neocheekiong <neocheekiong@gmail.com>
2020-05-19 22:11:20 +0800

    edited port settings

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const scientists = require('./models/index');
  * Configuration
  */
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 
 /**
